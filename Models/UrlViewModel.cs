b427dd651e6a16d29892bdfe48a656460f5c7ca0
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2024-08-23 13:22:59 +0300

    Implement AuthenticationController completely and major functionality of UrlController, create RegisterViewModel and UrlViewModel

---
+++ Models/UrlViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace InforceTestTask.Models
+{
+    public class UrlViewModel
+    {
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+        public string UserLogin { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public UrlViewModel() { }
+        public UrlViewModel(string originalUrl, string shortenedUrl, string userLogin, DateTime createdDate)
+        {
+            OriginalUrl = originalUrl;
+            ShortenedUrl = shortenedUrl;
+            UserLogin = userLogin;
+            CreatedDate = createdDate;
+        }
+    }
+}
